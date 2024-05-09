83ede5491f2f7e500782a085c32b3ee15c888d34
Yekna <urosjeknic@gmail.com>
2024-05-09 20:04:34 +0200

    make text white by default instead of relying on prefers-color-scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Map from "@/components/Map";
 
+// TODO: separate Map component and sidebar component so the sidebar doesn't rerender every time the map does
 export default function Home() {
   return <Map />;
 }
