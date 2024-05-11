886860598a6e975eb131f6d9b3b226bcc491c035
Yekna <urosjeknic@gmail.com>
2024-05-11 20:47:23 +0200

    Add icons for header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,40 @@ const Header = () => {
   return (
     <header className="z-50 fixed bottom-0 left-0 right-0 sm:static bg-gray-800 px-6 flex items-center justify-between h-16">
       <Link href="/">
-        <Image priority={true} alt="Weekendr Logo" src="logo.svg" width={50} height={50} />
+        <Image
+          priority={true}
+          alt="Weekendr Logo"
+          src="logo.svg"
+          width={50}
+          height={50}
+        />
       </Link>
       <nav>
-        <ul className="flex gap-3">
+        <ul className="flex gap-3 items-center">
           <li>
-            <Link href="/following">Clubs & Bars (Following)</Link>
+            <Link href="/following">
+              <Image
+                alt="Following"
+                src="/following.svg"
+                width={50}
+                height={50}
+              />
+            </Link>
           </li>
           <li>
-            <Link href="/register">Register</Link>
+            <Link href="/register">
+              <Image
+                alt="Register"
+                src="/register.svg"
+                width={50}
+                height={50}
+              />
+            </Link>
           </li>
           <li>
-            <Link href='/login'>Login</Link>
+            <Link href="/login">
+              <Image alt="Login" src="/login.svg" width={50} height={50} />
+            </Link>
           </li>
         </ul>
       </nav>
