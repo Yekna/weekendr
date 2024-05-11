a28b86ab609da7630b2cbd61b9ec17bea39099a6
Yekna <urosjeknic@gmail.com>
2024-05-11 20:51:38 +0200

    Display message if the user isn't following any venues

--- src/components/Parties.tsx
+++ src/components/Parties.tsx
@@ -33,25 +33,29 @@ const Parties = () => {
 
   return (
     <ul>
-      {data.map((venue) => (
-        <li className="bg-[#202e4b] p-5" key={venue.id}>
-          <Link href="#" className="flex gap-3 items-center">
-            <Image
-              src={venue.picture}
-              alt={venue.name}
-              width={100}
-              height={0}
-            />
-            <div className="flex flex-col">
-              <h2>{venue.name}</h2>
-              <p>{venue.tags}</p>
-              <p>{venue.genre}</p>
-              <p>{new Date(venue.date).toDateString()}</p>
-              <p>Venue: {venue.Venue.name}</p>
-            </div>
-          </Link>
-        </li>
-      ))}
+      {data.length ? (
+        data.map((venue) => (
+          <li className="bg-[#202e4b] p-5" key={venue.id}>
+            <Link href="#" className="flex gap-3 items-center">
+              <Image
+                src={venue.picture}
+                alt={venue.name}
+                width={100}
+                height={0}
+              />
+              <div className="flex flex-col">
+                <h2>{venue.name}</h2>
+                <p>{venue.tags}</p>
+                <p>{venue.genre}</p>
+                <p>{new Date(venue.date).toDateString()}</p>
+                <p>Venue: {venue.Venue.name}</p>
+              </div>
+            </Link>
+          </li>
+        ))
+      ) : (
+        <p>You aren&apos;t following any clubs/bars currently</p>
+      )}
     </ul>
   );
 };
